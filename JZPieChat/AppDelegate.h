29e1543b8561f151538c03313056b43ae2aa86f6
JacyFox <JacyFox@foxmail.com>
2016-03-30 20:12:09 +0800

    Initial Commit

---
+++ JZPieChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZPieChat
+//
+//  Created by Admin on 16/3/30.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZPieChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZPieChat
+//
+//  Created by Admin on 16/3/30.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
